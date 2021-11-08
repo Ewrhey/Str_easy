92310c01855f43306dea0b08365d4e8a31df5b43
Ewrhey <92299233+Ewrhey@users.noreply.github.com>
2021-11-08 12:45:53 +0300

    Update string_func_1.cpp

--- string_func_1.cpp
+++ string_func_1.cpp
@@ -44,22 +44,19 @@ return str1;
 }
 
 string itc_cmp_str(string str1, string str2, int num){
-if (str2 == ""){
-return str1;
-}
-string str = "";
-if (num == 0){
-    str += str2;
-    str += itc_slice_str(str1, itc_len(str2), itc_len(str1));
-}
-else{
-str += itc_slice_str(str1, 0, num-1);
-str += itc_slice_str(str2, 0, (itc_len(str1) - num) - 1);;
-if (itc_len(str) < itc_len(str1)){
-    str += itc_slice_str(str1, num + itc_len(str2), itc_len(str1));
-}
-}
-return str;
+if (str2 == ""){ 
+return str1; 
+} 
+string str = ""; 
+if (num == 0){ 
+    str += str2; 
+    str += str1; 
+    return itc_slice_str(str, 0, itc_len(str1)-1); 
+} 
+str += itc_slice_str(str1, 0, num-1); 
+str += str2; 
+str += itc_slice_str(str1, num, itc_len(str1)); 
+return itc_slice_str(str, 0, itc_len(str1)-1); 
 }
 
 
